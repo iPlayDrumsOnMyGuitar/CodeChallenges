e910083a0335d9b61b96dc0d708b2463d056d40b
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2021-11-12 16:22:40 -0600

    coding challenge

---
+++ isItAPalindrome?.js
@@ -0,0 +1,10 @@
+/* Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+ */
+
+function isPalindrome(x) {
+ 
+    return x.toLowerCase() == x.split('').reverse().join('').toLowerCase()
+ 
+}
