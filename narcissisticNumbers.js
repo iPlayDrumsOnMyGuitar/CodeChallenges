f3a309360da83a5a4ce731cc20e1e100f0b546fb
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2021-12-13 21:47:15 -0600

    coding challenge

--- narcissisticNumbers.js
+++ narcissisticNumbers.js
@@ -1,6 +1,5 @@
 /* Narcissistic Numbers
 
-
 A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems confusing, refer to the example below.
 
 Ex: 153, where l = 3 ( the number of digits in 153 )
