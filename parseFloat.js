6d2cc645e96a939336373f09ae892e78c628a3e0
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2021-12-19 16:18:30 -0600

    coding challenge

---
+++ parseFloat.js
@@ -0,0 +1,10 @@
+/* 
+Parse float
+
+Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+ */
+function parseF(s) {
+  
+    return parseFloat(s) ? parseFloat(s) : (s==0 && s!==false) ? 0: null
+ 
+}
