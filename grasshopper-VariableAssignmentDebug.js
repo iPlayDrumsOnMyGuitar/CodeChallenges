85408f310af26de820ac44d612b8cb477dc3ae9c
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2022-01-27 14:17:59 -0600

    coding challenge

---
+++ grasshopper-VariableAssignmentDebug.js
@@ -0,0 +1,10 @@
+/* Grasshopper - Variable Assignment Debug
+
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+ */
+var a = "dev"
+
+var b = "Lab"
+
+var name = a + b
