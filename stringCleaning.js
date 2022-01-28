195f3cfd3997912f381f366b69603329d6f3d3c4
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2022-01-28 14:14:03 -0600

    coding challenge

---
+++ stringCleaning.js
@@ -0,0 +1,29 @@
+/* String cleaning
+
+Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+Examples (input -> output)
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+ */
+function stringClean(s){
+  
+    let ans = ''
+    
+    let arr = [...s]
+    
+    for (i = 0; i < arr.length; i++){
+      
+      let char = arr[i]
+      
+      if(isNaN(char) || char == ' '){
+        
+        ans += char
+      
+      }
+    
+    }return ans
+  
+  }
