44518ffd80b4f44bf5f6580a35e5efa5aeb33c95
iplaydrumsonmyguitar <gadomski.brian@gmail.com>
2022-02-16 16:31:46 -0600

    ez coding challenge

---
+++ simpleMultiplication.js
@@ -0,0 +1,18 @@
+/* Simple multiplication
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+ */
+function simpleMultiplication(number) {
+    
+    if(number % 2 === 0){
+      
+        number *= 8
+    
+    }else{
+      
+        number *= 9
+    
+    }
+  
+    return number
+}
