3433dee91b842c42019e543eb58bb99a86ffcbb8
iplaydrumsonmyguitar <gadomski.brian@gmail.com>
2022-03-21 14:15:28 -0500

    coding challenges

---
+++ toSquare(Root)OrNotToSquare(Root).js
@@ -0,0 +1,28 @@
+/* To square(root) or not to square(root)
+
+Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null.
+ */
+function squareOrSquareRoot(array) {
+    let newArray = []
+    for(let i = 0; i <= array.length -1; i++){
+     
+      if(Math.sqrt(array[i]) % 1 ===0){
+        newArray.push(Math.sqrt(array[i]))
+      }else{
+        newArray.push(array[i] * array[i])
+      }
+     // if(Math.sqrt(array[i]) % 0 
+    }
+    
+    return newArray;  
+  }
+  
