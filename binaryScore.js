e6898c32560ec85928e1a589d92544ec5b279b5b
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2022-05-28 10:32:39 -0500

    coding challenge

---
+++ binaryScore.js
@@ -0,0 +1,18 @@
+/* Binary scORe
+
+Objective
+Given a number n we will define its scORe to be 0 | 1 | 2 | 3 | ... | n, where | is the bitwise OR operator.
+
+Write a function that takes n and finds it's scORe.
+
+n	scORe n
+0	0
+1	1
+49	63
+1000000	1048575
+ */
+function score(n){
+ 
+    return n ? parseInt(n.toString(2).replace(/0/g, '1'),2) : 0
+
+}
