c3e524c417c20b2c3ba651db7fbb4b18227f4dd8
iPlayDrumsOnMyGuitar <gadomski.brian@gmail.com>
2022-10-25 12:22:30 -0500

    coding challenge

---
+++ stringTransformer.js
@@ -0,0 +1,23 @@
+/* String transformer
+
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE" */
+
+function stringTransformer(str) {
+    return str
+      .split(' ')
+      .reverse()
+      .join(' ')
+      .split('')
+      .map(v => v == v.toUpperCase() ?
+        v.toLowerCase() :
+        v.toUpperCase())
+      .join('');
+  }
