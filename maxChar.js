2ff531f1b58b69fcf946154411099238b2935f3b
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-01-09 07:06:01 -0600

    coding challenge

---
+++ maxChar.js
@@ -0,0 +1,44 @@
+/*  
+Given a string of characters, return the character that appears the most often.
+
+describe("Max Character", () => {
+ it("Should return max character", () => {
+  assert.equal(max("Hello World!"), "l");
+ });
+});
+
+No String or Array Methods (well brute force it first, but then no methods)!  */
+
+/* function maxCharacter(str){
+    let countObj = {}
+    for(const l of str){
+        if(!countObj[l]){
+            countObj[l] = 1
+        }else{
+            countObj[l]++
+        }
+    }for(l of countObj){
+
+    }
+}
+
+console.log(maxCharacter('Hello World'))
+ */
+
+function maxCharacter(str){
+    let charMap = {},
+        count = 0,
+        maxChar = null
+
+    for (const char of str){
+        charMap[char] = charMap[char] + 1 || 1
+    }
+    for(const char in charMap){
+        if(charMap[char] > count){
+            count = charMap[char]
+            maxChar = char
+        }
+    }return maxChar
+}
+
+console.log(maxCharacter('Hello World'), 'l')
