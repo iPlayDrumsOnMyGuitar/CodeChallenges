ba2d3b75a1de6ba3910ee55c2fbebf9b79c2a73b
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-01-17 22:40:10 -0600

    coding challenge

--- recursion#2Fibonacci.js
+++ recursion#2Fibonacci.js
@@ -23,3 +23,16 @@
     }
     return fibonacci(n-1) + fibonacci(n-2)
   };
+
+  /* function fib(n){
+    let memo = []
+    if (memo[n] != null){
+        return memo[n]
+    }else if(n == 1 || n == 2){
+        result = 1
+    }else{
+        result = fib(n-1) + fib(n-2)
+            memo[n] = result
+        
+    }return result
+} */
