499138efa27de5a349f7367e77a23cf341dd882c
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-01-20 00:57:42 -0600

    code challenge

--- 4-27-22.js
+++ 4-27-22.js
@@ -11,9 +11,15 @@ countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10] */
 
 function countBy(x, n) {
-    let z = [];
+    
+  let z = [];
+  
   for(let i = x; i <= (x * n); i+=x){
+  
     z.push(i)
+  
   }
-    return z;
-  }
+  
+  return z;
+  
+}
