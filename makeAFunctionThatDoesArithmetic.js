5b77497fe3bfe177599144708f295d67747fd6a5
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-03-06 08:18:59 -0600

    code challenge

--- makeAFunctionThatDoesArithmetic.js
+++ makeAFunctionThatDoesArithmetic.js
@@ -26,3 +26,16 @@ function arithmetic(a, b, operator){
       return a / b
   }
     }
+
+    function arithmetic(a, b, operator){
+      switch(operator) {
+        case 'add':
+          return a + b;
+        case 'subtract':
+          return a - b;
+        case 'multiply':
+          return a * b;
+        case 'divide':
+          return a / b;
+      }
+    }
