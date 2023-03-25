2350df6881f1dd31352a16e9f231cfa4cfc0056e
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-03-25 08:06:39 -0500

    code challenge

--- areTheNumbersInOrder.js
+++ areTheNumbersInOrder.js
@@ -15,18 +15,11 @@ inAscOrder([1,6,10,18,2,4,20]); // returns false
 inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order */
 
 function inAscOrder(arr) {
-   
     for(let i = 0; i < arr.length - 1; i++) {
-   
         if (arr[i] > arr[i+1]) {
-   
             return false; 
-   
         }
-   
     }
-   
     return true;
-  
 }
   
