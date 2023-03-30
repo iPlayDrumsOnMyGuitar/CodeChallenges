3c68e0214a446d44fbfe9a1d9b0c5e1d8782cbc3
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-03-30 08:06:40 -0500

    code challenge

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -8,13 +8,5 @@ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
  */
 
 function createPhoneNumber(numbers){
-    var format = "(xxx) xxx-xxxx";
-    
-    for(var i = 0; i < numbers.length; i++)
-    {
-      format = format.replace('x', numbers[i]);
-    }
-    
-    return format;
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
   }
-  
