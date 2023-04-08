a732f7c1dcb90988ff153dcb58070d88a40c7b13
iPlayDrumsOnMyGuitar <payne.in.the.brian@gmail.com>
2023-04-08 01:02:04 -0500

    code challenge

---
+++ breakCamelCase.js
@@ -0,0 +1,21 @@
+/* Break camelCase
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+ */
+
+function solution(string) {
+    let res = "";
+    let letters = string.split("");
+    letters.forEach(letter=>{
+      if(letter.toUpperCase() == letter)
+        res+=" "+letter
+      else
+        res+=letter
+    })
+    return res;
+  }
